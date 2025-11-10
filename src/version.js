64e305bee8d9ad1896724151b387408c3e57a405
Zac-Chen-2024 <zacchen2024@gmail.com>
2025-11-10 18:11:00 -0500

    备份: 11.10第一次开发前 - 准备添加遮罩颜色编辑功能

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 // 自动生成的版本信息文件
 // 每次构建时更新
 export const VERSION = '1.5.0';
-export const BUILD_TIME = '2025-11-04T20:24:15.596Z';
-export const BUILD_NUMBER = 1762287855597;
+export const BUILD_TIME = '2025-11-04T20:25:01.399Z';
+export const BUILD_NUMBER = 1762287901400;
