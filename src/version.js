6e8fc86920b3dac9585a7254839f7fb595426c3b
root <root@filetranslation.cloud.local>
2025-11-23 21:22:40 +0000

    fix: 强制实体表格滚动，设置max-height 400px

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 // 自动生成的版本信息文件
 // 每次构建时更新
 export const VERSION = '1.5.0';
-export const BUILD_TIME = '2025-11-20T20:52:09.612Z';
-export const BUILD_NUMBER = 1763671929612;
+export const BUILD_TIME = '2025-11-23T21:10:13.209Z';
+export const BUILD_NUMBER = 1763932213209;
